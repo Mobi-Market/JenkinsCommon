5a274149bcad43835bf197af8cbb54c610f7265b
Scott-David Jones <scott.jones@autumndev.co.uk>
2018-08-23 10:14:24 +0100

    making copied scripts executable
    Signed-off-by: Scott-David Jones <scott.jones@autumndev.co.uk>

--- vars/copyGlobalLibraryScript.groovy
+++ vars/copyGlobalLibraryScript.groovy
@@ -4,5 +4,7 @@ def call(String srcPath, String destPath = null) {
   destPath = destPath ?: createTempLocation(srcPath)
   writeFile file: destPath, text: libraryResource(srcPath)
   echo "copyGlobalLibraryScript: copied ${srcPath} to ${destPath}"
+  sh "chmod +x ${${destPath}}"
+  echo "added executable permissions to ${destPath}"
   return destPath
 }
--- vars/createTempLocation.groovy
+++ vars/createTempLocation.groovy
@@ -4,17 +4,3 @@ def call(String path) {
   String tmpDir = pwd tmp: true
   return tmpDir + File.separator + new File(path).getName()
 }
-
-/**
-  * Returns the path to a temp location of a script from the global library (resources/ subdirectory)
-  *
-  * @param srcPath path within the resources/ subdirectory of this repo
-  * @param destPath destination path (optional)
-  * @return path to local file
-  */
-String copyGlobalLibraryScript(String srcPath, String destPath = null) {
-  destPath = destPath ?: createTempLocation(srcPath)
-  writeFile file: destPath, text: libraryResource(srcPath)
-  echo "copyGlobalLibraryScript: copied ${srcPath} to ${destPath}"
-  return destPath
-}
