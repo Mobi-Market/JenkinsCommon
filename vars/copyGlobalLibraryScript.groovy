11a8d3478164389814fd340b8dea90fd95952dbc
Scott-David Jones <scott.jones@autumndev.co.uk>
2018-08-23 10:15:34 +0100

    woops
    Signed-off-by: Scott-David Jones <scott.jones@autumndev.co.uk>

--- vars/copyGlobalLibraryScript.groovy
+++ vars/copyGlobalLibraryScript.groovy
@@ -4,7 +4,7 @@ def call(String srcPath, String destPath = null) {
   destPath = destPath ?: createTempLocation(srcPath)
   writeFile file: destPath, text: libraryResource(srcPath)
   echo "copyGlobalLibraryScript: copied ${srcPath} to ${destPath}"
-  sh "chmod +x ${${destPath}}"
+  sh "chmod +x ${destPath}"
   echo "added executable permissions to ${destPath}"
   return destPath
 }
