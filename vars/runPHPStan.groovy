56063c07f218b556ef716378ce208442f495da54
Scott-David Jones <scott.jones@autumndev.co.uk>
2018-10-24 15:17:23 +0100

    adding in publishing of reports
    Signed-off-by: Scott-David Jones <scott.jones@autumndev.co.uk>

--- vars/runPHPStan.groovy
+++ vars/runPHPStan.groovy
@@ -2,4 +2,6 @@
 
 def call(Map config) {
 	sh(copyGlobalLibraryScript('phpstan.sh'))
+	def checkstyle = scanForIssues tool: [$class: 'CheckStyle'], pattern: 'reports/**/phpstan.checkstyle*.xml'
+	publishIssues id: 'PHPStan', name: 'Code Sniff (Full)', issues:[checkstyle],  healthy: 10000, unHealthy: 9000
 }
