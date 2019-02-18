675304a58ae25616b8558b23201ea423092b2050
Scott-David Jones <scott.jones@autumndev.co.uk>
2019-02-18 21:40:16 +0000

    tag integration with rc. and live with v.

--- resources/tagging.sh
+++ resources/tagging.sh
@@ -1,9 +1,17 @@
 if ["$BRANCH_NAME" == "master"]
 then
 	echo pushing tag...
-	git tag -a $BUILD_NUMBER -m "release $BUILD_NUMBER"
+	git tag -a v.$BUILD_NUMBER -m "release $BUILD_NUMBER"
+	git push origin --tags
+	echo pushing tag...Done
+fi
+
+if ["$BRANCH_NAME" == "integration"]
+then
+	echo pushing tag...
+	git tag -a rc.$BUILD_NUMBER -m "release candidate $BUILD_NUMBER"
 	git push origin --tags
 	echo pushing tag...Done
 else
-	echo Not Master branch, skipping tagging.
+	echo Not Master or Integration branch, skipping tagging.
 fi
