07b0fc1f4e6367246305bad07b9fd325e2c5c0a4
Scott-David Jones <scott.jones@autumndev.co.uk>
2019-02-19 11:01:36 +0000

    updating to correct scanForIssues command as per jenkins

--- vars/runPHPCpd.groovy
+++ vars/runPHPCpd.groovy
@@ -2,6 +2,6 @@
 
 def call(Map config) {
 	sh(copyGlobalLibraryScript('phpcpd.sh'))
-	def checkstyle = scanForIssues pattern: 'reports/**/phpcpd.pmd.xml', reportEncoding: '', sourceCodeEncoding: '', tool: [$class: 'Pmd']
+	def checkstyle = scanForIssues tool: pmdParser(pattern: 'reports/**/phpcpd.pmd.xml')
 	publishIssues id: 'copyPaste', name: 'Copy Paste Detection', issues:[checkstyle],  healthy: 10000, unHealthy: 9000
 }
--- vars/runPHPCs.groovy
+++ vars/runPHPCs.groovy
@@ -2,6 +2,6 @@
 
 def call(Map config) {
 	sh(copyGlobalLibraryScript('phpcs.sh'))
-	def checkstyle = scanForIssues tool: [$class: 'CheckStyle'], pattern: 'reports/**/phpcs.checkstyle*.xml'
+	def checkstyle = scanForIssues tool: checkStyle(pattern: 'reports/**/phpcs.checkstyle*.xml')
 	publishIssues id: 'checkstyleFull', name: 'Code Sniff (Full)', issues:[checkstyle],  healthy: 10000, unHealthy: 9000
 }
