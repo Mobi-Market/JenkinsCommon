085e9bcff9c9807d0da06b16bcc82ce7444115b3
Scott-David Jones <scott.jones@autumndev.co.uk>
2019-02-27 10:22:37 +0000

    coverage reports

--- vars/runFunctionalTests.groovy
+++ vars/runFunctionalTests.groovy
@@ -15,4 +15,12 @@ def call(Map config) {
 	// publishIssues id: 'functionaltests', name: 'Function Tests', issues:[checkstyle],  healthy: 10000, unHealthy: 9000
 	// junit 'Reports/**/JenkinsFunctional-phpunit-junit.xml'
 	xunit([PHPUnit(deleteOutputFiles: true, failIfNotNew: true, pattern: 'reports/JenkinsDatabase-phpunit-junit.xml', skipNoTestFiles: false, stopProcessingIfError: false)])
+	step([
+		$class: 'CloverPublisher',
+		cloverReportDir: 'reports/coverage/database',
+		cloverReportFileName: 'reports/coverage/JenkinsDatabase-coverage.xml',
+		healthyTarget: [methodCoverage: 70, conditionalCoverage: 80, statementCoverage: 80],
+		unhealthyTarget: [methodCoverage: 50, conditionalCoverage: 50, statementCoverage: 50],
+		failingTarget: [methodCoverage: 0, conditionalCoverage: 0, statementCoverage: 0]
+	])
 }
