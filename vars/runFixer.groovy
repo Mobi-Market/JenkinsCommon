5e90fa7e84e126d37fc172888640f5dae5b0139b
Scott-David Jones <scott.jones@autumndev.co.uk>
2019-03-01 18:22:59 +0000

    adding some sane metrics

--- vars/runFixer.groovy
+++ vars/runFixer.groovy
@@ -3,5 +3,5 @@
 def call(Map config) {
 	sh(copyGlobalLibraryScript('phpcsf.sh'))
 	def checkstyle = scanForIssues tool: checkStyle(pattern: 'reports/fixer.checkstyle.xml')
-	publishIssues id: 'checkstyleFull', name: 'Code Fixer', issues:[checkstyle],  healthy: 10000, unHealthy: 9000
+	publishIssues id: 'checkstyleFull', name: 'Code Fixer', issues:[checkstyle],  healthy: 25, unHealthy: 26
 }
--- vars/runPHPStan.groovy
+++ vars/runPHPStan.groovy
@@ -3,5 +3,5 @@
 def call(Map config) {
 	sh(copyGlobalLibraryScript('phpstan.sh'))
 	def checkstyle = scanForIssues tool: checkStyle(pattern: 'reports/phpstan.checkstyle.xml')
-	publishIssues id: 'PHPStan', name: 'PHP Stan', issues:[checkstyle],  healthy: 10000, unHealthy: 11000
+	publishIssues id: 'PHPStan', name: 'PHP Stan', issues:[checkstyle],  healthy: 20, unHealthy: 21
 }
