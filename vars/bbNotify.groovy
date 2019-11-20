c1bf41c6e5cf422cb60e0c53df12b0525239dc2a
Scott-David Jones <scott.jones@autumndev.co.uk>
2019-11-20 12:25:37 +0000

    change: remove slack plugins
    Signed-off-by: Scott-David Jones <scott.jones@autumndev.co.uk>

--- vars/bbNotify.groovy
+++ vars/bbNotify.groovy
@@ -13,11 +13,9 @@ def call(Map config, Closure body) {
 	try {
 		body()
 		bitbucketStatusNotify( buildKey: buildKey, buildName: BuildName, buildState: 'SUCCESSFUL')
-		slackSend channel: system, color: 'good', iconEmoji: '', message: 'Build Success', teamDomain: 'autumndev', tokenCredentialId: 'Slack', username: 'jenkins'
 	}
 	catch (err) {
 		bitbucketStatusNotify( buildKey: buildKey,buildName: BuildName, buildState: 'FAILED', buildDescription: err.toString())
-		slackSend channel: system, color: 'danger', iconEmoji: '', message: 'Build Failed', teamDomain: 'autumndev', tokenCredentialId: 'Slack', username: 'jenkins'
 		throw err
 	}
 }
