eb0a2031e91a34afbd1366539fd69f7de32dec19
Scott-David Jones <scott@mobi-market.co.uk>
2020-12-24 12:46:49 +0000

    more testing

--- vars/runUnitTests.groovy
+++ vars/runUnitTests.groovy
@@ -15,12 +15,12 @@ def call(Map config) {
 	// publishIssues id: 'unittests', name: 'Unit Tests', issues:[checkstyle],  healthy: 10000, unHealthy: 9000
 	// junit 'Reports/**/JenkinsUnit-phpunit-junit.xml'
 	xunit([PHPUnit(deleteOutputFiles: true, failIfNotNew: false,  pattern: 'reports/JenkinsUnit-phpunit-junit.xml', skipNoTestFiles: false, stopProcessingIfError: false)])
-	step([
-		$class: 'CloverPublisher',
-		cloverReportDir: 'reports/coverage/unit',
-		cloverReportFileName: 'reports/coverage/JenkinsUnit-coverage.xml',
-		healthyTarget: [methodCoverage: 70, conditionalCoverage: 80, statementCoverage: 80],
-		unhealthyTarget: [methodCoverage: 50, conditionalCoverage: 50, statementCoverage: 50],
-		failingTarget: [methodCoverage: 0, conditionalCoverage: 0, statementCoverage: 0]
-	])
+	// step([
+	// 	$class: 'CloverPublisher',
+	// 	cloverReportDir: 'reports/coverage/unit',
+	// 	cloverReportFileName: 'reports/coverage/JenkinsUnit-coverage.xml',
+	// 	healthyTarget: [methodCoverage: 70, conditionalCoverage: 80, statementCoverage: 80],
+	// 	unhealthyTarget: [methodCoverage: 50, conditionalCoverage: 50, statementCoverage: 50],
+	// 	failingTarget: [methodCoverage: 0, conditionalCoverage: 0, statementCoverage: 0]
+	// ])
 }
